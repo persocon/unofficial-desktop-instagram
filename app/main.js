9080d269654706dca9eddb5d760dcbe6f43b39a7
Pedro <pedro@i2x.ai>
2017-09-29 10:25:41 +0200

    prettier working?

--- app/main.js
+++ app/main.js
@@ -9,22 +9,25 @@ const { BrowserWindow, session } = require('electron');
 let win;
 
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 412, height: 732, fullscreenable: false });
+    // Create the browser window.
+    win = new BrowserWindow({ width: 412, height: 732, fullscreenable: false });
 
-  // and load the index.html of the app.
-  win.loadURL('https://www.instagram.com/', { userAgent: 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 5 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.99 Mobile Safari/537.36' });
+    // and load the index.html of the app.
+    win.loadURL('https://www.instagram.com/', {
+        userAgent:
+            'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 5 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.99 Mobile Safari/537.36',
+    });
 
-  // Open the DevTools.
-  // win.webContents.openDevTools();
+    // Open the DevTools.
+    // win.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -34,17 +37,17 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
 });
